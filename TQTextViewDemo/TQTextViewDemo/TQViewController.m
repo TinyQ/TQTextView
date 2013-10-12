f745904fd7104f0fde09cfe9899db28e63938739
fuqiang <443054369@qq.com>
2013-10-12 17:47:55 +0800

    add image

--- TQTextViewDemo/TQTextViewDemo/TQViewController.m
+++ TQTextViewDemo/TQTextViewDemo/TQViewController.m
@@ -20,7 +20,7 @@
     [super viewDidLoad];
     self.view.backgroundColor = [UIColor grayColor];
     
-    TQTextView *textView = [[TQTextView alloc] initWithFrame:CGRectMake(0, 50, 320, 200)];
+    TQTextView *textView = [[TQTextView alloc] initWithFrame:CGRectMake(0, 40, 320, 200)];
     textView.placeholder = @"请输入内容";
     textView.font = [UIFont systemFontOfSize:16];
     textView.maxTextLength = 11;
